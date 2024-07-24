9a3cce634d7ed87ad49868e859845231f12ecb70
Taha <dev.taha29@gmail.com>
2024-07-24 18:57:08 +0530

    feat: Implemented SplashActivity

--- app/src/main/java/com/trycatchprojects/gharkharch/screens/SplashActivity.kt
+++ app/src/main/java/com/trycatchprojects/gharkharch/screens/SplashActivity.kt
@@ -1,10 +1,14 @@
 package com.trycatchprojects.gharkharch.screens
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.trycatchprojects.gharkharch.MainActivity
 import com.trycatchprojects.gharkharch.R
 import com.trycatchprojects.gharkharch.databinding.ActivitySplashBinding
 
@@ -15,5 +19,9 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, OnBoardingActivity::class.java))
+            finish()
+        },1000)
     }
 }

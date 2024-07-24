39aa5bf927ba64de738ad06ee81a0f8f03edae30
Taha <dev.taha29@gmail.com>
2024-07-24 22:25:55 +0530

    feat: Implemented AddFragment U.I

--- app/src/main/java/com/trycatchprojects/gharkharch/fragments/AddFragment.kt
+++ app/src/main/java/com/trycatchprojects/gharkharch/fragments/AddFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.trycatchprojects.gharkharch.R
 import com.trycatchprojects.gharkharch.databinding.FragmentAddBinding
 
@@ -15,6 +17,24 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentAddBinding.inflate(layoutInflater)
+        val list= listOf("Youtube","Light Bill","Water Bill","Electricity Bill","Disney Hotstar","Groceries")
+        val arrayAdapter=ArrayAdapter(requireContext(),
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,list)
+        arrayAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item)
+        binding.spinnerName.adapter=arrayAdapter
+
+        binding.spinnerName.onItemSelectedListener=object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem=parent?.getItemAtPosition(position).toString()
+//                binding.textView5.text=selectedItem
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+
         return binding.root
     }
 

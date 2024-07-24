44cf154977a6eaa5e393bc6f21ed71377df606d7
Taha <dev.taha29@gmail.com>
2024-07-24 20:05:58 +0530

    feat: Implemented OnBoardingActivity , check for stored name after SplashActivity

--- app/src/main/java/com/trycatchprojects/gharkharch/screens/OnBoardingActivity.kt
+++ app/src/main/java/com/trycatchprojects/gharkharch/screens/OnBoardingActivity.kt
@@ -1,19 +1,37 @@
 package com.trycatchprojects.gharkharch.screens
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.trycatchprojects.gharkharch.MainActivity
 import com.trycatchprojects.gharkharch.R
 import com.trycatchprojects.gharkharch.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOnBoardingBinding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+
+        binding.btnGetStarted.setOnClickListener {
+            val myName = binding.edtMyName.text.toString()
+
+            if (myName.isNotEmpty() && myName.length <=12) {
+                sharedPreferences.edit().putString("myName", myName).apply()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                // Show an error message or handle the case where the input is invalid
+                binding.edtMyName.error = "Name must be exactly 12 characters"
+            }
+        }
     }
 }
--- app/src/main/java/com/trycatchprojects/gharkharch/screens/SplashActivity.kt
+++ app/src/main/java/com/trycatchprojects/gharkharch/screens/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.trycatchprojects.gharkharch.screens
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -14,14 +15,22 @@ import com.trycatchprojects.gharkharch.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, OnBoardingActivity::class.java))
+            val myName = sharedPreferences.getString("myName", null)
+            val nextActivity = if (myName.isNullOrEmpty()) {
+                OnBoardingActivity::class.java
+            } else {
+                MainActivity::class.java
+            }
+            startActivity(Intent(this, nextActivity))
             finish()
-        },1000)
+        }, 1000)
     }
 }
